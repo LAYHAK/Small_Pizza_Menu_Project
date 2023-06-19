274dc2a0650c93a1224606797f05f04cf176ca26
LAYHAK <layhak.garu@gmail.com>
2023-06-19 11:03:59 +0700

    Finish Pizza Order app UI

--- src/index.js
+++ src/index.js
@@ -1,17 +1,169 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const pizzaData = [
+	{
+		name: 'Focaccia',
+		ingredients: 'Bread with italian olive oil and rosemary',
+		price: 6,
+		photoName: 'pizzas/focaccia.jpg',
+		soldOut: false,
+	},
+	{
+		name: 'Pizza Margherita',
+		ingredients: 'Tomato and mozarella',
+		price: 10,
+		photoName: 'pizzas/margherita.jpg',
+		soldOut: false,
+	},
+	{
+		name: 'Pizza Spinaci',
+		ingredients: 'Tomato, mozarella, spinach, and ricotta cheese',
+		price: 12,
+		photoName: 'pizzas/spinaci.jpg',
+		soldOut: false,
+	},
+	{
+		name: 'Pizza Funghi',
+		ingredients: 'Tomato, mozarella, mushrooms, and onion',
+		price: 12,
+		photoName: 'pizzas/funghi.jpg',
+		soldOut: false,
+	},
+	{
+		name: 'Pizza Salamino',
+		ingredients: 'Tomato, mozarella, and pepperoni',
+		price: 15,
+		photoName: 'pizzas/salamino.jpg',
+		soldOut: true,
+	},
+	{
+		name: 'Pizza Prosciutto',
+		ingredients: 'Tomato, mozarella, ham, aragula, and burrata cheese',
+		price: 18,
+		photoName: 'pizzas/prosciutto.jpg',
+		soldOut: false,
+	},
+];
 
+function App() {
+	return (
+		<div className='container'>
+			<Header />
+			<Menu />
+			<Footer />
+		</div>
+	);
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
+function Header() {
+	return (
+		<header className='header'>
+			<h1> Pizza Company </h1>
+		</header>
+	);
+}
+function Menu() {
+	const pizzas = pizzaData;
+	// const pizzas = [];
+	const numPizzas = pizzas.length;
+	return (
+		<main className='menu'>
+			<h2>Our Menu</h2>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+			{/*?  using ternary operator */}
+			{/* {numPizzas > 0 && (
+				<ul className='pizzas'>
+					{pizzas.map((pizza) => (
+						<Pizza
+							pizzaObj={pizza}
+							key={pizza.name}
+						/>
+					))}
+				</ul>
+			)} */}
+
+			{numPizzas > 0 ? (
+				<>
+					<p>
+						Authentic Italian cuisine. 6 creative dishes to choose
+						from. ALl from our stone oven, all organick all
+						delicious.
+					</p>
+					<ul className='pizzas'>
+						{pizzas.map((pizza) => (
+							<Pizza
+								pizzaObj={pizza}
+								key={pizza.name}
+							/>
+						))}
+					</ul>
+				</>
+			) : null}
+
+			{/* <Pizza
+				name={pizzaData[0].name}
+				ingredient={pizzaData[0].ingredients}
+				photoName={pizzaData[0].photoName}
+				price={16}
+			/> */}
+		</main>
+	);
+}
+
+function Pizza({ pizzaObj }) {
+	return (
+		<li className={pizzaObj.soldOut ? 'pizza sold-out' : 'pizza'}>
+			<img
+				src={pizzaObj.photoName}
+				alt={pizzaObj.name}></img>
+			<div>
+				<h3>{pizzaObj.name}</h3>
+				<p>{pizzaObj.ingredients}</p>
+				<span>{pizzaObj.soldOut ? 'SOLD OUT' : pizzaObj.price}</span>
+			</div>
+		</li>
+	);
+}
+function Footer() {
+	const hour = new Date().getHours();
+	const openHour = 12;
+	const closeHour = 22;
+	const isOpen = hour >= openHour && hour <= closeHour;
+	// if (hour >= openHour && hour <= closeHour) alert(`We're Open!!`);
+	// else alert(`We're Close!`);
+
+	return (
+		<footer className='footer'>
+			{isOpen ? (
+				<Order
+					closeHour={closeHour}
+					openHour={openHour}
+				/>
+			) : (
+				<p>
+					We're Happy to welcome you between {openHour}:00 and
+					{closeHour}:00
+				</p>
+			)}
+		</footer>
+	);
+}
+function Order({ closeHour, openHour }) {
+	return (
+		<>
+			<div className='order'>
+				<p>
+					We're Open From {openHour}:00 to {closeHour}:00 Come visit
+					us or order online
+				</p>
+				<button className='btn'>Order Now</button>
+			</div>
+		</>
+	);
+}
